45b922ce9baab0728f2b257eb7f112ed1b125ca0
Derek Cabrera <78906554+DeeRo-dev@users.noreply.github.com>
2024-03-20 19:28:08 -0300

    bugs
    Bug en el texto del layout Res.

--- src/pages/Flota/LayoutFlota.tsx
+++ src/pages/Flota/LayoutFlota.tsx
@@ -5,15 +5,17 @@ export default function LayoutFlota() {
   return (
     <>
       <div className="w-11/12 m-auto mt-5 flex flex-col">
-        <div className="pl-15">
+        <div className="flex  items-center">
+        <div className="pl-15 ml-5 text-colorText">
           <Link to="/" className="flex items-center">
             <FaArrowLeftLong />
             <p className="ml-2">Volver</p>
           </Link>
         </div>
-        <h2 className="text-xl ml-15 font-semibold text-colorText m-auto">
+        <h2 className="text-xl ml-15 font-semibold text-colorText m-auto ">
           Nueva Reserva
         </h2>
+        </div>
         <Outlet />
       </div>
     </>
